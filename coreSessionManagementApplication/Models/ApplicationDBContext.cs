e7c4934b525c3956621a16ce2a8e86a82da17b98
Supriya Hegde <shegde@dovercorp.com>
2021-11-08 15:48:42 +0530

    first commit

---
+++ coreSessionManagementApplication/Models/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace coreSessionManagementApplication.Models
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public DbSet<Orderdetails> orderdetails { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+        //public DbSet<PizzaView> PizzaViews { get; set; }
+        public DbSet<order> orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=supriyaadmin.database.windows.net;User Id=supadmin;Password=admi@12345678;Database=JoesPizzaDB;Trusted_Connection=False;Encrypt=True;");
+        }
+    }
+}
